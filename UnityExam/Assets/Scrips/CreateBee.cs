1ae6814e7b8ae46034eb2a73104631d2ea3057fe
VideKallmanHIS <a25vidka@student.his.se>
2025-11-21 11:22:29 +0100

    Added Instatiate Prefab Bee

--- UnityExam/Assets/Scrips/CreateBee.cs
+++ UnityExam/Assets/Scrips/CreateBee.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class CreateBee : MonoBehaviour
 {
+    public GameObject bee;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instantiate(bee,transform.position,transform.rotation);
     }
 
     // Update is called once per frame
