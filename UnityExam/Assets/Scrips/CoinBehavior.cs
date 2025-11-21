134d465786e75369e41d5c86f789fd54b55dd59e
VideKallmanHIS <a25vidka@student.his.se>
2025-11-21 10:52:34 +0100

    Changed Coin Prefab to have 3D music behavior

--- UnityExam/Assets/Scrips/CoinBehavior.cs
+++ UnityExam/Assets/Scrips/CoinBehavior.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinBehavior : MonoBehaviour
 {
     private CircleCollider2D circleCollider2D;
+    private SpriteRenderer spriteRenderer;
+    public UnityEvent onDeath;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         circleCollider2D = GetComponent<CircleCollider2D>();
     }
 
@@ -19,7 +23,9 @@ public class CoinBehavior : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            Destroy(this.gameObject);
+            circleCollider2D.enabled = false;
+            spriteRenderer.color = new Color(1, 1, 1, 0);
+            onDeath.Invoke();
         }
     }
 }
