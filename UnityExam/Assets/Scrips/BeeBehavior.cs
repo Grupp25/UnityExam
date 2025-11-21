36883c1e50dfe2e836bf819ae398be76ff6a1f52
VideKallmanHIS <a25vidka@student.his.se>
2025-11-21 11:09:22 +0100

    Added enemy bee projectile

--- UnityExam/Assets/Scrips/BeeBehavior.cs
+++ UnityExam/Assets/Scrips/BeeBehavior.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class BeeBehavior : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
+    private float moveSpeed = 2f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
     }
 }
--- UnityExam/Assets/Scrips/CreateBee.cs
+++ UnityExam/Assets/Scrips/CreateBee.cs
@@ -13,4 +13,9 @@ public class CreateBee : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+        
+    }
 }
